3ac9a6b06877e0023d626a0775db064235e297e9
David92p <dave.panetta92@gmail.com>
2022-05-19 13:13:56 +0200

    modified page layout and added background image

--- js/script.js
+++ js/script.js
@@ -1,26 +1,47 @@
-//let name = prompt('Hello! What\'s your name? ')
 
-let higher = document.getElementById('higher');
-let lower = document.getElementById('lower');
+let name = prompt('Hello! What\'s your name? ', '')
+let welcome = document.getElementById('welcome')
+
+let regards = `Welcome ${name}! `
+
+welcome.textContent += regards
+
+
+let increment = document.getElementById("increment");
+let decrement = document.getElementById('decrement');
 let reset = document.getElementById('reset');
-let number = document.getElementById('number')
+let counter = document.getElementById('counter')
 
+let number = 0;
 
-let count = 0;
-
+// this function increases the counter value by one unit
 let incrementCounter = ()=>{
-    count += 1;
-    number.innerHTML = count
+    number += 1;
+    counter.innerHTML = number
 };
 
+
+// this function decreases the counter value by one unit 
+// a small condition is set to prevent the counter from becoming negative
 let decrementCounter = ()=>{
-    count += 1;
-    number.innerHTML = count;
+    if (number == 0){
+        number = 0
+        counter.innerHTML = number
+    }else{
+        number -= 1;
+        counter.innerHTML = number
+    }   
 };
 
+// this function resets the counter value to zero
 let resetCounter = ()=>{
-    count = 0;
-    number.innerHTML = count
+    number = 0;
+    counter.innerHTML = number
 };
 
-higher.addEventListener('click', incrementCounter())
+
+increment.addEventListener('click', incrementCounter);
+
+decrement.addEventListener("click", decrementCounter);
+
+reset.addEventListener("click", resetCounter);
